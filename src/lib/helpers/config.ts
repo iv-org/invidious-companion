974d03cf9a1c711c5694db5461af7f87852ba65c
Fijxu <fijxu@nadeko.net>
2025-05-21 13:08:05 -0400

    change configuration name and env variable

--- src/lib/helpers/config.ts
+++ src/lib/helpers/config.ts
@@ -5,8 +5,8 @@ export const ConfigSchema = z.object({
     server: z.object({
         port: z.number().default(Number(Deno.env.get("PORT")) || 8282),
         host: z.string().default(Deno.env.get("HOST") || "127.0.0.1"),
-        uds: z.boolean().default(
-            Deno.env.get("SERVER_UDS") === "true" || false,
+        use_unix_socket: z.boolean().default(
+            Deno.env.get("SERVER_USE_UNIX_SOCKET") === "true" || false,
         ),
         secret_key: z.string().length(16).default(
             Deno.env.get("SERVER_SECRET_KEY") || "",
