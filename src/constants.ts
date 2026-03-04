1786e154979c7dda4b051da490007e687af23d81
Ashley :3 <ashley@ggtylerr.dev>
2026-03-04 22:54:54 +0300

    Set PLAYER_ID to a specific value (#275)

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,8 @@
 // rolls out a new Player. This is helpful when Youtube.JS is not able to
 // extract the signature decipher algorithm and we need to wait for a fix
 // in Youtube.JS.
-export const PLAYER_ID = undefined;
+// taken from https://github.com/yt-dlp/yt-dlp/commit/d3165e83ffc0088eef5e594927ea9ac99a6e2ce6
+export const PLAYER_ID = "9f4cc5e4"; 
 
 // Error message shown when tokenMinter is not yet ready
 export const TOKEN_MINTER_NOT_READY_MESSAGE =
