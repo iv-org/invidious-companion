b67eeb4d24337051e870a4b42c9c7dd98d649cd6
Émilien (perso) <4016501+unixfox@users.noreply.github.com>
2026-03-11 19:56:46 +0100

    chore: use PR https://github.com/LuanRT/YouTube.js/pull/1148 (#287)
    * chore: use PR https://github.com/LuanRT/YouTube.js/pull/1148
    
    * fix PO token validation search filters
    
    * chore: use fixed player id 01bde470

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,7 @@
 // rolls out a new Player. This is helpful when Youtube.JS is not able to
 // extract the signature decipher algorithm and we need to wait for a fix
 // in Youtube.JS.
-// taken from https://github.com/yt-dlp/yt-dlp/commit/d3165e83ffc0088eef5e594927ea9ac99a6e2ce6
-export const PLAYER_ID = "9f4cc5e4"; 
+export const PLAYER_ID = "01bde470"; 
 
 // Error message shown when tokenMinter is not yet ready
 export const TOKEN_MINTER_NOT_READY_MESSAGE =
